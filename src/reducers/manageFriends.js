c36916df0e0eb2891906179add81557452f5c4dd
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-26 03:35:46 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            let newFriends = [...state.friends]
+            newFriends.push(action.friend)
+            return {friends: newFriends}
+        case "REMOVE_FRIEND":
+            let filteredFriends = state.friends.filter(friend => friend.id !== action.id)
+            return {friends: filteredFriends}
+        default:
+            return state
+    }
 }
